89d381ee624fbdfdb4443893ea0c135493592c19
EekshithEedala <Eekshith369@gmail.com>
2026-08-19 11:12:16 +0530

    Added my-maven-project

---
+++ src/main/java/SE_2n/my_maven_project/App.java
@@ -0,0 +1,13 @@
+package SE_2n.my_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
